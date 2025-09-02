34d8a03265089eaeb21f5d185a8de403d2718e04
Mohammad Faqusa <103820177+mohammad-faqusa@users.noreply.github.com>
2025-09-02 20:37:11 +0300

    17. Outlook

---
+++ section-1-introduction/17-outlook/note.md
@@ -0,0 +1,8 @@
+## 17. Outlook
+
+### congratulations 
+
+### prepared for next chapter 
+* principles of linux 
+* explore terminal 
+
--- titleConverter.py
+++ titleConverter.py
@@ -58,7 +58,7 @@ def create_code_exercise(section_title, part_title):
 section_title = "section 1: introduction"
 # section_title = "quizzes"
 
-part_title = "16. How to create a Snapshot? [VirtualBox]"
+part_title = "17. Outlook"
 print(f"export PART_TITLE='{part_title}'")
 
 prepare_resources(section_title, part_title)
