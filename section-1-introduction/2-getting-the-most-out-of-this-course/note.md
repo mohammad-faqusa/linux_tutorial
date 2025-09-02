81e09c72846e7dc3000fee49e45728e54e7bd21d
Mohammad Faqusa <103820177+mohammad-faqusa@users.noreply.github.com>
2025-09-02 14:21:43 +0300

    2. Getting the Most out of this Course

---
+++ section-1-introduction/2-getting-the-most-out-of-this-course/note.md
@@ -0,0 +1,14 @@
+## 2. Getting the Most out of this Course
+
+
+### what is your goad ? 
+* work with severs ? 
+* deploy applications 
+* certifacates 
+* personal projects
+* as primary operating system ? 
+
+
+### what is the best approach 
+* follow the course structure
+
--- titleConverter.py
+++ titleConverter.py
@@ -58,7 +58,7 @@ def create_code_exercise(section_title, part_title):
 section_title = "section 1: introduction"
 # section_title = "coding-exercises"
 
-part_title = "1. intro"
+part_title = "2. Getting the Most out of this Course"
 print(f"export PART_TITLE='{part_title}'")
 
 prepare_resources(section_title, part_title)
