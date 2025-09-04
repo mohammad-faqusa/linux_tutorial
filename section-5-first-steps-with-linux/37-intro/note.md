3934704eca673cafd54bd8d1af618da8199c6eab
Mohammad Faqusa <103820177+mohammad-faqusa@users.noreply.github.com>
2025-09-04 17:57:06 +0300

    37. Intro

---
+++ section-5-first-steps-with-linux/37-intro/note.md
@@ -0,0 +1,10 @@
+## 37. Intro
+
+### tasks 
+
+### first steps with linux 
+* user management 
+* elevating privileges sudo 
+* package management 
+  * keep the system up to date 
+  * install and remove software
--- titleConverter.py
+++ titleConverter.py
@@ -55,10 +55,10 @@ def create_code_exercise(section_title, part_title):
     # print(part_directory_path)
     create_directory(part_directory_path)
 
-section_title = "Section 4: First Steps in the Terminal"
+section_title = "Section 5: First Steps with Linux"
 # section_title = "quizzes"
 
-part_title = "36. Getting Terminal Help: `man` and `--help`"
+part_title = "37. Intro"
 print(f"export PART_TITLE='{part_title}'")
 
 prepare_resources(section_title, part_title)
