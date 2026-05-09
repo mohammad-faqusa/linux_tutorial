07c29a88fe0dffa7a4dc5170c650d5e59b42b465
mohammadfaqusa <201014@ppu.edu.ps>
2026-05-09 20:39:32 +0300

    178. Materials regarding the Upcoming Lessons

---
+++ section-16-linux-processes/178-materials-regarding-the-upcoming-lessons/note.md
@@ -0,0 +1,8 @@
+## 178. Materials regarding the Upcoming Lessons
+
+
+In the next few lessons, we will download a few files. For this, you’ll need the wget program.
+
+If you want to reproduce the download, feel free to use this link, which is also used in the next video:
+
+    https://downloads.codingcoursestv.eu/055%20-%20bash/wget/download-slow
--- titleConverter.py
+++ titleConverter.py
@@ -58,7 +58,7 @@ def create_code_exercise(section_title, part_title):
 section_title = "Section 16: Linux Processes"
 # section_title = "quizzes"
 
-part_title = "177. Influencing Processes with Signals"
+part_title = "178. Materials regarding the Upcoming Lessons"
 print(f"export PART_TITLE='{part_title}'")
 
 prepare_resources(section_title, part_title)
